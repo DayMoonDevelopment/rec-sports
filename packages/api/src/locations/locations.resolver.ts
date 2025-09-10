21f17f026ce79e40f9fe488ab3e5d04ef64192b6
matt rothenberger <matt@daymoon.io>
2025-09-10 16:11:09 -0400

    check in

--- packages/api/src/locations/locations.resolver.ts
+++ packages/api/src/locations/locations.resolver.ts
@@ -1,13 +1,18 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, ResolveField, Resolver, Parent } from '@nestjs/graphql';
 
 import { LocationsArgs } from './dto/locations.args';
+import { FacilitiesService } from './facilities/facilities.service';
 import { LocationsService } from './locations.service';
+import { Facility } from './facilities/models/facility.model';
 import { Location } from './models/location.model';
 import { LocationsConnection } from './models/locations-connection.model';
 
-@Resolver()
+@Resolver(() => Location)
 export class LocationsResolver {
-  constructor(private readonly locationsService: LocationsService) {}
+  constructor(
+    private readonly locationsService: LocationsService,
+    private readonly facilitiesService: FacilitiesService,
+  ) {}
 
   @Query(() => LocationsConnection)
   async locations(@Args() args: LocationsArgs): Promise<LocationsConnection> {
@@ -17,5 +22,10 @@ export class LocationsResolver {
   @Query(() => Location, { nullable: true })
   async location(@Args('id') id: string): Promise<Location | null> {
     return this.locationsService.findLocationById(id);
+  }
+
+  @ResolveField(() => [Facility], { nullable: true })
+  async facilities(@Parent() location: Location): Promise<Facility[]> {
+    return this.facilitiesService.findFacilitiesByLocationId(location.id);
   }
 }
